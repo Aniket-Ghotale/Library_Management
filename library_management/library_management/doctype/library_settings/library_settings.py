a2f13d2563b49ce2afccee190f30cb8a04435131
aniket-ghotale <aniketghotale2014@gmail.com>
2021-10-18 12:57:40 +0530

    first commit

---
+++ library_management/library_management/doctype/library_settings/library_settings.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, alexa and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class LibrarySettings(Document):
+	pass
